c521004536ebb7ed0c3e858d8ce85d1674210d0d
Vadim Semenov <vsemenov@leongaming.com>
2020-07-07 21:09:15 +0300

    simple prax - example commit

---
+++ src/com/buseduc/javacourse/oop2020/topic_01/task_00_vs/TestTask.java
@@ -0,0 +1,15 @@
+package com.buseduc.javacourse.oop2020.topic_01.task_00_vs;
+
+public class TestTask {
+
+    /**
+     * @author vs
+     * Тема 1. ОСНОВЫ ООП
+     * Задание 0.
+     * Это образец задания, его нельзя выбирать!
+     */
+
+    public static void main(String[] args) {
+        // здесь начинается выполнение задания
+    }
+}
